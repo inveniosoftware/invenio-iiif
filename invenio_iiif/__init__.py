a6a36db3845b64c48be765437c25c3cb3d90d9dc
Jan Okraska <jan.okraska@cern.ch>
2019-05-23 15:39:20 +0200

    docs: content improvements
    * link to configuration variables from flask-IIIF
    * Closes #15
    
    Signed-off-by: Jan Okraska <jan.okraska@cern.ch>
    Co-authored-by: Chris Topaloudis <topless@gmail.com>
    Co-authored-by: Nicola Tarocco <ntarocco@gmail.com>

--- invenio_iiif/__init__.py
+++ invenio_iiif/__init__.py
@@ -20,6 +20,85 @@ authorization check and file retrieval to
 `Invenio-Files-REST <https://invenio-files-rest.rtfd.io>`_ and it serves the
 image after adaptation by Flask-IIIF.
 
+Basics
+------
+
+Preview
+~~~~~~~
+
+Invenio-IIIF can be used in a combination with
+`Invenio-Previewer <https://invenio-previewer.rtfd.io>`_ to preview images.
+It provides an extension to preview the most common image formats and it is
+exposed via the entry point named ``iiif_image``.
+
+The template used to render the image can be configured with the configuration
+``IIIF_PREVIEW_TEMPLATE``.
+
+To use it with an existing Invenio instance you should update your
+``PREVIEWER_PREFERENCE`` configuration by adding ``iiif_image`` to the list of
+available previewers with the desired precedence order. For example:
+
+.. code-block:: python
+
+    PREVIEWER_PREFERENCE = [
+        'iiif_image',
+        'json_prismjs',
+        'xml_prismjs',
+        'pdfjs',
+        'zip',
+    ]
+
+Caching
+~~~~~~~
+
+Image caching is provided by ``Flask-IIIF``. You can change cache expiration
+by setting the ``IIIF_CACHE_TIME``.
+
+.. code-block:: python
+
+    # 60 seconds * 60 (1 hour) * 24 (1 day)
+    IIIF_CACHE_TIME = 60 * 60 * 24
+
+By default ``Flask-IIIF`` caches images in memory. You can cache images on
+Redis by using the available ``ImageRedisCache`` handler (providing Redis URL)
+or point to your own custom handler by setting it in ``IIIF_CACHE_HANDLER``.
+
+.. code-block:: python
+
+    # Cache handler
+    IIIF_CACHE_HANDLER = 'flask_iiif.cache.redis:ImageRedisCache'
+
+    # Redis URL
+    IIIF_CACHE_REDIS_URL = 'redis://localhost:6379/0'
+
+
+PDF cover
+~~~~~~~~~
+
+When the retrieved file is a PDF and with ImageMagick/Wand installed in your
+environment (see ``Configuration`` documentation), then a cover image with the
+first page of the PDF file can be generated and served it as preview.
+
+Authorization
+~~~~~~~~~~~~~
+
+Permissions to retrieve the requested images are delegated to
+`Invenio-Files-REST <https://invenio-files-rest.rtfd.io>`_. At each request,
+authorization is checked to ensure that the user has sufficient privileges.
+
+Thumbnails
+~~~~~~~~~~
+
+The module can pre-cache thumbnails of requested images. It provides a celery
+task that will fetch a given image and resize it to create a thumbnail. It is
+then cached so it can be served efficiently.
+
+.. code-block:: python
+
+    from invenio_iiif.tasks import create_thumbnail
+    create_thumbnail(image_key, '250')
+
+
 Initialization
 --------------
 
@@ -29,16 +108,10 @@ First create a Flask application:
 >>> app = Flask('myapp')
 >>> app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
 
-Configuration
-~~~~~~~~~~~~~
-
-Then, You can define the IIIF API prefix by setting the Invenio configuration
+Then, you can define the IIIF API prefix by setting the Invenio configuration
 ``IIIF_UI_URL``.
 
 >>> app.config.update(IIIF_UI_URL='/iiif-demo')
-
-Usage
------
 
 The example below will demonstrate how Invenio-IIIF works with a simple image.
 First, initialize the needed Invenio extensions:
@@ -81,7 +154,7 @@ Now, add a few sample images:
 >>> db.session.commit()
 
 Serving an image
-~~~~~~~~~~~~~~~~
+----------------
 
 While Flask-IIIF requires the UUID of the image to retrieve as part of the URL,
 Invenio needs the bucket id, version id and the key of the file so that it can
@@ -104,44 +177,6 @@ you can create the corresponding IIIF URL:
 
 The result will be
 ``/iiif-demov2/<bucket_id>:<version_id>:img.png/full/full/0/default.png``
-
-PDF cover
-~~~~~~~~~
-
-If the file is a PDF and ImageMagick is installed in your system, then the
-module can extract the first page of the PDF, create a ``png`` from it and
-serve it as cover page.
-
-Authorization
-~~~~~~~~~~~~~
-
-Permissions to retrieve the requested images are delegated to
-`Invenio-Files-REST <https://invenio-files-rest.rtfd.io>`_. At each request,
-authorization is checked to ensure that the user has sufficient privileges.
-
-Preview
-~~~~~~~
-
-Invenio-IIIF can be used in a combination with
-`Invenio-Previewer <https://invenio-previewer.rtfd.io>`_ to preview images.
-It provides an extension to preview the most common image formats and it is
-exposed via the entry point named ``iiif_image``.
-
-The template used to render the image can be configured with the configuration
-``IIIF_PREVIEW_TEMPLATE``.
-
-Thumbnails
-~~~~~~~~~~
-
-The module can precache thumbnails of requested images. It provides a celery
-task that will fetch a given image and resize it to create a thumbnail. It is
-then cached so it can be served efficiently.
-
-.. code-block:: python
-
-    from invenio_iiif.tasks import create_thumbnail
-    create_thumbnail(image_key, '250')
-
 """
 
 from __future__ import absolute_import, print_function
